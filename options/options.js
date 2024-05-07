20d0436efabb7bcf66dcf372eb49ba5197291a14
郎 需兴 <langxuxing@gmail.com>
2024-05-07 14:37:49 +0800

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "aaqa-47828",
-  url: "https://aaqa-47828.botics.co",
-  api: "https://aaqa-47828.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "aaqa-47828",
+  "url": "https://aaqa-47828.botics.co",
+  "api": "https://aaqa-47828.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "modules"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
